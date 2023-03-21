3a0cf200938966c11a5f1fe6bfdfd199f12dcb6b
Joel Davenport <joelmike46@gmail.com>
2023-03-21 12:02:55 -0400

    wip

--- app/routers/chats.py
+++ app/routers/chats.py
@@ -49,6 +49,8 @@ def handle_message_creation(bot_id, chat_id, messageJson, db, current_user):
 
     newChatJson = newChatData.__dict__
 
+    print("-----HERE-----")
+
     newChatJson["chat_messages"] = newChatData.chat_messages
 
     chatJson = jsonable_encoder(Chat(**newChatJson))
