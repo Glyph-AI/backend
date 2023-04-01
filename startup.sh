e56f6321815c5b6a50389430f0aadbbb7df5b899
Joel Davenport <joelmike46@gmail.com>
2023-04-01 08:38:25 -0400

    run app alongside worker code for gcp

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 if [ $IS_WORKER = "true" ];
 then
     echo "RUNNING WORKER"
-    echo $(celery --app=app.worker worker --loglevel=info)
+    echo $(celery --app=app.worker worker --loglevel=info & uvicorn --factory app.main:create_app --host 0.0.0.0)
 else
     echo "BOOTING APP"
     echo $(python app/database_setup.py)
