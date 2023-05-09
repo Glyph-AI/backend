ca426208a333f457a02be35080417a2eaa5d53b1
Joel Davenport <joelmike46@gmail.com>
2023-05-08 20:29:33 -0400

    try this

--- app/prompts/base.py
+++ app/prompts/base.py
@@ -29,10 +29,10 @@ FORMAT INSTRUCTIONS:
 When responding, please respond in the following format:
 
 ```
-{{
+{
     "action"": $ACTION, -- The action you should take based on your thought. If there are multiple steps required, this should be the first one in order.
     "action_input": $ACTION_INPUT -- The input to the tool based on the user's input in plain english
-}}
+}
 ```
 
 Based on your list, respond with the first action, action_input sequence required to address the user’s request. ONLY ONE SEQUENCE should be included in the response. Response MUST BE IN THE ABOVE FORMAT. Nothing else should be returned
