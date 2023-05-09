f7f6db8befe0be52590cf3032b515151c622fb54
Joel Davenport <joelmike46@gmail.com>
2023-05-09 12:38:39 -0400

    handle no information issues with document search

--- app/services/tools/document_search.py
+++ app/services/tools/document_search.py
@@ -11,4 +11,6 @@ class DocumentSearch(BaseTool):
             BotText.include_in_context == True, BotText.bot_id == self.bot_id
         ).order_by(Embedding.vector.l2_distance(embed)).limit(3).all()
         context = [i.content for i in top]
+        if len(context) == 0:
+            return ["NO INFORMATION FOUND"]
         return context
