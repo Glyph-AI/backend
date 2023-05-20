ecfc92a8a69a9a6746b958443750609c40ebbdbe
Joel Davenport <joel.davenport@ntient.ai>
2023-05-20 12:26:02 -0400

    try this

--- app/services/sentence_transformer_service.py
+++ app/services/sentence_transformer_service.py
@@ -1,11 +1,10 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("/embedding_model")
-
 class SentenceTransformerService():
     def __init__(self):
+        self.model = SentenceTransformer("/embedding_model")
         pass
 
     def get_embedding(self, text: str):
         # returns a list of length 768
-        return model.encode(text)
+        return self.model.encode(text)
