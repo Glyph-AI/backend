beafa728c1a94bef9805d0fbbc17bfaac94a850e
Joel Davenport <joel.davenport@ntient.ai>
2023-05-20 12:47:08 -0400

    don't have reload enabled

--- startup.sh
+++ startup.sh
@@ -7,5 +7,10 @@ else
     echo "BOOTING APP"
     echo $(python app/database_setup.py)
     echo $(alembic upgrade head)
-    uvicorn --factory app.main:create_app --reload --host 0.0.0.0
+    if [ $ENVIRONMENT = "development"];
+    then
+        uvicorn --factory app.main:create_app --reload --host 0.0.0.0
+    else
+        uficorn --factory app.main:create_app --host 0.0.0.0
+    fi
 fi
