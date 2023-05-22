cf3f8fa2a8ca9f324ce137c6de28fd621c84698b
Joel Davenport <joel.davenport@ntient.ai>
2023-05-22 16:55:18 -0400

    not sure if this will work

--- app/db/session.py
+++ app/db/session.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 import os
 
-engine = create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True)
+engine = create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
