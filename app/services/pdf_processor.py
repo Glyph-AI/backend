47be3a53a11ec511916fae50fab5b3978e7b99e0
Joel Davenport <joelmike46@gmail.com>
2023-05-23 10:01:54 -0400

    serious optimization

--- app/services/pdf_processor.py
+++ app/services/pdf_processor.py
@@ -1,4 +1,5 @@
 import pdf2image
+from PyPDF2 import PdfReader
 try:
     from PIL import Image
 except ImportError:
@@ -17,25 +18,54 @@ class PdfProcessor():
 
     def process(self, filepath):
         # read pdf in
-        pdf_info = pdf2image.pdfinfo_from_path(filepath)
-        pages = pdf_info['Pages']
-        print(f"Total Pages: {pages}")
-        chunk_size = 10
-        chunks = self.__chunk_generator(pages, chunk_size)
-        c_time = datetime.now()
-        with PyTessBaseAPI() as api:
+        # try to read it as a searchable pdf first
+        searchable = False
+        reader = PdfReader(filepath)
+        pages_to_check = 5
+        if len(reader.pages) < 5:
+            pages_to_check = len(reader.pages)
+        for i in range(pages_to_check):
+            page = reader.pages[i]
+            text = page.extract_text()
+
+            if text != "":
+                # we can read this PDF as searchable
+                searchable = True
+                break
+
+        if searchable:
+            print("PROCESSING PDF AS DIGITAL")
+            pages = len(reader.pages)
+            c_time = datetime.now()
             with open("/temp/output.txt", "w+") as f:
-                for idx in chunks:
-                    start = idx
-                    end = idx + (chunk_size - 1)
-                    images = pdf2image.convert_from_path(
-                        filepath, dpi=100, first_page=start, last_page=end)
-                    for image in images:
+                for i in range(pages):
+                    page = reader.pages[i]
+                    text = page.extract_text()
+                    f.write(text)
 
-                        api.SetImage(image)
-                        page_text = api.GetUTF8Text()
-                        f.write(page_text)
+            print(f"TIME ELAPSED: {datetime.now() - c_time}")
 
-        print(f"TIME ELAPSED: {datetime.now() - c_time}")
+        else:
+            print("PROCESSING PDF AS SCANNED")
+            pdf_info = pdf2image.pdfinfo_from_path(filepath)
+            pages = pdf_info['Pages']
+            print(f"Total Pages: {pages}")
+            chunk_size = 10
+            chunks = self.__chunk_generator(pages, chunk_size)
+            c_time = datetime.now()
+            with PyTessBaseAPI() as api:
+                with open("/temp/output.txt", "w+") as f:
+                    for idx in chunks:
+                        start = idx
+                        end = idx + (chunk_size - 1)
+                        images = pdf2image.convert_from_path(
+                            filepath, dpi=100, first_page=start, last_page=end)
+                        for image in images:
+
+                            api.SetImage(image)
+                            page_text = api.GetUTF8Text()
+                            f.write(page_text)
+
+            print(f"TIME ELAPSED: {datetime.now() - c_time}")
 
         return "/temp/output.txt"
--- startup.sh
+++ startup.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-if [ $IS_WORKER = "true" ];
-then
+env_constant="development"
+if [ $IS_WORKER = "true" ]; then
     echo "RUNNING WORKER"
-    echo $(celery --app=app.worker worker --loglevel=info & uvicorn --factory app.main:create_app --host 0.0.0.0)
+    if [ "$ENVIRONMENT" = "development" ]; then
+        echo "IN DEVELOPMENT"
+        echo $(celery --app=app.worker worker --loglevel=info)
+    else
+        echo $(celery --app=app.worker worker --loglevel=info & uvicorn --factory app.main:create_app --host 0.0.0.0)
+    fi
+    
 else
     echo "BOOTING APP"
     echo $(python app/database_setup.py)
     echo $(alembic upgrade head)
-    uvicorn --factory app.main:create_app --reload --host 0.0.0.0
+    if [ "$ENVIRONMENT" = "development" ]; then
+        echo "IN DEVELOPMENT"
+        uvicorn --factory app.main:create_app --reload --host 0.0.0.0
+    else
+        uvicorn --factory app.main:create_app --host 0.0.0.0
+    fi
 fi
