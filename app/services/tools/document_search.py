38f7f2a4be2498995ad282e71ea8d179e4d0169b
Joel Davenport <joelmike46@gmail.com>
2023-05-30 13:01:00 -0400

    fix bug with document search

--- app/services/tools/document_search.py
+++ app/services/tools/document_search.py
@@ -9,7 +9,7 @@ class DocumentSearch(BaseTool):
         embed = sts.get_embedding(message)
         top = self.db.query(Embedding).join(Text).join(BotText).filter(
             BotText.include_in_context == True, BotText.bot_id == self.bot_id
-        ).order_by(Embedding.vector.l2_distance(embed)).limit(3).all()
+        ).order_by(Embedding.vector.l2_distance(embed).desc()).limit(3).all()
         context = [i.content for i in top]
         if len(context) == 0:
             return ["NO INFORMATION FOUND"]
