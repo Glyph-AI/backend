4725d27455c832cb469bdfdd9703251b4e0fdb2d
Joel Davenport <joelmike46@gmail.com>
2023-06-13 09:18:25 -0400

    try to stop respond to user from generating urls

--- app/prompts/respond_to_user.py
+++ app/prompts/respond_to_user.py
@@ -4,4 +4,6 @@ Based on the responses from tools, and the plan above respond to the user's init
 USER INPUT: {user_input}
 
 DO NOT FOLLOW action, action_response formatting instructions. Generate whatever text you think is necessary to respond to the user, including relevant information that may not have been directly requested. Be thorough in explaining concepts. Cite your source only if requested.
+
+DO NOT INCLUDE ANY URLS TO EXTERNAL SITES.
 """
