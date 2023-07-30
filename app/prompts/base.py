4a33f67b3355592857af3a5ba3e353d07411eceb
Joel Davenport <joel.davenport@ntient.ai>
2023-07-30 14:15:49 -0400

    move to few-shot prompting

--- app/prompts/base.py
+++ app/prompts/base.py
@@ -11,11 +11,24 @@ CHAT HISTORY:
 
 TOOLS: {tools}
 
-Given the below instructions, identify the component problems involved in solving the user’s request taking into account the CHAT HISTORY
+Given the below instructions and examples, identify the component problems involved in solving the user’s request taking into account the CHAT HISTORY.
 
 Identify sub-problems that are dependent on other sub-problems and note the sub-problems each is dependent on. List them in order such that no problem is being solved before the problem's it depends on are solved.
 
 You cannot ask the user for additional information, so do your best to solve the problem with this list. 
+
+EXAMPLES:
+---
+User: Can you generate recipes based on the items on my shopping list note?
+Response: 
+    Document Search - Get the items from the user's shopping list
+    Respond to User - Generate recipes with the items from the shopping list
+
+User: Based on the book I've uploaded, can you generate marketing strategies for my mobile app?
+Response:
+    Document Search - Search for marketing strategy information from the uploaded book
+    Google Search - Search Google for general information on marketing for mobile apps
+    Respond to User - Generate a strategy for the user
 
 RESPONSE INSTRUCTIONS: Respond with a list in the following format
 ---
@@ -43,5 +56,26 @@ When responding, please respond in the following format:
 }
 ```
 
-Based on your list, respond with the first action, action_input sequence required to address the user’s request. ONLY ONE SEQUENCE should be included in the response. Response MUST BE IN THE ABOVE FORMAT. Nothing else should be returned
+EXAMPLE:
+---
+Action List: 
+    Document Search - Get the items from the user's shopping list
+    Respond to User - Generate recipes with the items from the shopping list
+Response 1:
+    ```
+        {
+            "action": "Document Search",
+            "action_input": "Shopping List"
+        }
+    ```
+Response 2:
+    ```
+        {
+            "action": "Respond to User",
+            "action_input": "Generate recipes that use these items: $ITEMS"
+        }
+    ```
+---
+
+Based on your action list, respond with the first action, action_input sequence required to address the user’s request. ONLY ONE SEQUENCE should be included in the response. Response MUST BE IN THE ABOVE FORMAT. Nothing else should be returned
 """
