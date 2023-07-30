2e3cdf57abb0b214763ff0d05236b4ada9d28051
Joel Davenport <joel.davenport@ntient.ai>
2023-07-30 14:27:25 -0400

    get rid of examples in conversation prompt

--- app/prompts/conversation.py
+++ app/prompts/conversation.py
@@ -12,28 +12,6 @@ TOOL_RESPONSE:
 {tool_response}
 ---
 
-EXAMPLE:
----
-Action List: 
-    Document Search - Get the items from the user's shopping list
-    Respond to User - Generate recipes with the items from the shopping list
-Action 1:
-    ```
-        {
-            "action": "Document Search",
-            "action_input": "Shopping List"
-        }
-    ```
-Tool Response: '- Chicken - Bell Peppers - Rice'
-Response:
-    ```
-        {
-            "action": "Respond to User",
-            "action_input": "Generate recipes that use these items: Chicken, Bell Peppers, Rice"
-        }
-    ```
----
-
 Based on your list, and last action, and the response from the tool please respond with an action, action_input pair for the next action.
 
 """
